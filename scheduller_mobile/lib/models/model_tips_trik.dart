d7507052910a7739bc36147414e24574caa01bb5
Nabila Adinda <nabilaadinda23@gmail.com>
2023-05-08 13:14:34 +0700

    change emilado

--- scheduller_mobile/lib/models/model_tips_trik.dart
+++ scheduller_mobile/lib/models/model_tips_trik.dart
@@ -8,7 +8,7 @@ final List<TipsTrik> image = [
   TipsTrik(image: 'assets/tips_trik/hal_2.png'),
   TipsTrik(image: 'assets/tips_trik/hal_3.png'),
   TipsTrik(image: 'assets/tips_trik/hal_4.png'),
-  TipsTrik(image: 'assets/tips_trik/hal_5.png'),
+  TipsTrik(image: 'assets/tips_trik/hal_5.jpeg'),
   TipsTrik(image: 'assets/tips_trik/hal_6.png'),
   TipsTrik(image: 'assets/tips_trik/hal_7.png'),
   TipsTrik(image: 'assets/tips_trik/hal_8.png'),
